05c912a6aa49a3019b1052271b8f80b81a9e6e77
dawood@123 <dawoodeshraff123@gmail.com>
2026-05-26 22:06:31 +0530

    implemented eslint and prettier

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.status(200).send('Welcome to the Acquisitions API' );
-}); 
+  res.status(200).send('Welcome to the Acquisitions API');
+});
 
 export default app;
